52fa87ac5b2075335335c70782f000533e6c67f5
flaviodsilverio <flavio.silverio@headforwards.com>
2016-06-10 17:26:32 +0100

    added the adding item functionality

--- Shopping Cart/ShoppingCart.swift
+++ Shopping Cart/ShoppingCart.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ShoppingCart: UITableViewController {
     
+    var shoppingItems = [String]()
+    //Array<String>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -25,16 +28,44 @@ class ShoppingCart: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return shoppingItems.count
     }
     
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "myFirstCell   ")
         
-        cell.textLabel?.text = "This will be my first iOS App"
+        cell.textLabel?.text = shoppingItems[indexPath.row]
         
         return cell
     }
     
+    @IBAction func addButtonPressed(sender: AnyObject) {
+        
+        let addItemAlertController = UIAlertController(title: "Add Item", message: "Please insert the name of the new shopping item:", preferredStyle: .Alert)
+        
+        addItemAlertController.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+            
+            textField.placeholder = "Shopping Item Name"
+            
+        })
+        
+        addItemAlertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+        
+        addItemAlertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
+            (action) in
+            
+            if(addItemAlertController.textFields![0].text!.characters.count > 0){
+                
+                    self.shoppingItems.append(addItemAlertController.textFields![0].text!)
+                    self.tableView.reloadData()
+
+            }
+        
+        }))
+        
+        self.presentViewController(addItemAlertController, animated: false, completion: nil)
+    }
+    
 }
+
